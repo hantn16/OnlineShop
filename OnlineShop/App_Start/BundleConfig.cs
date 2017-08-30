0cbb72be8bb50c24df9b5f7883f727bf149347b2
Han Trinh <hantn16@gmail.com>
2017-08-30 22:26:33 +0700

    300817-1026

--- OnlineShop/App_Start/BundleConfig.cs
+++ OnlineShop/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace OnlineShop
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/umd/popper.js",
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
